d1129f90d6583be2f201bc3998bf7f229337bb62
Payns98 <payns98@gmail.com>
2025-12-07 22:13:31 +0100

    Deploy React App

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,10 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-// Beispielwerte eintragen: deine echte Supabase URL und PUBLIC ANON KEY
 export const supabase = createClient(
   sb_publishable_SodatXc5OM_7kTrXUadaGA__ASDxs65F,
   eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2dW9ya2pwdHdpc2dqdXNrdWhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4NzkwNDEsImV4cCI6MjA4MDQ1NTA0MX0.Y343ojdZTKpeUp2y6jYpWp_BZ0aGns4FwGGxBKgd9q0
 )
+import { createClient } from '@supabase/supabase-js'
+
+
