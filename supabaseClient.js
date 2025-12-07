f52247721e19f2f008afec3b709b2ac3a85d6c2e
payns98 <payns98@googlemail.com>
2025-12-07 15:27:01 +0100

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,8 @@
 // supabaseClient.js
+
 import { createClient } from "@supabase/supabase-js";
 
-// Hier deine eigenen Keys eintragen:
-const supabaseUrl = "https://qvuorkjptwisgjuskuhh.supabase.co";       // z.B. https://xyz.supabase.co
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2dW9ya2pwdHdpc2dqdXNrdWhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4NzkwNDEsImV4cCI6MjA4MDQ1NTA0MX0.Y343ojdZTKpeUp2y6jYpWp_BZ0aGns4FwGGxBKgd9q0";       // z.B. dein public anon key
+const supabaseUrl = "https://qvuorkjptwisgjuskuhh.supabase.co";     // z. B. https://xyz.supabase.co
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2dW9ya2pwdHdpc2dqdXNrdWhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4NzkwNDEsImV4cCI6MjA4MDQ1NTA0MX0.Y343ojdZTKpeUp2y6jYpWp_BZ0aGns4FwGGxBKgd9q0";     // dein anon key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
